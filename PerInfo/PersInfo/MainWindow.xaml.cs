7a50e433503432efa647494b3e47374d2e827186
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-02-25 23:26:34 -0800

    PersInfo

---
+++ PerInfo/PersInfo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> PersInfo { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersInfo = new();
+            PersInfo.Add(new Person() { Name = "Punith", Locality = "Bangalore", Org = "Cinema" });
+            PersInfo.Add(new Person() { Name = "Yash", Locality = "Bangalore", Org = "Cinema" });
+            this.DataContext= this;
+        }
+    }
+}
