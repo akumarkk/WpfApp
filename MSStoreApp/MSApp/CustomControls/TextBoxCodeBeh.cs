74032371a70fd731ea3f08068c8ed320a0432656
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-08 00:11:33 -0800

    cc with cb

---
+++ MSStoreApp/MSApp/CustomControls/TextBoxCodeBeh.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace MSApp.CustomControls
+{
+    public class TextBoxCodeBeh: System.Windows.Controls.UserControl
+    {
+        TextBlock _label;
+        private TextBox _textBox;
+        private StackPanel _panel;
+
+        TextBox text = new TextBox()
+        {
+            Text = "Hello from Yash!"
+        };
+
+        public TextBoxCodeBeh()
+        {
+            
+            //Content = text;
+            _panel = new StackPanel();
+            _panel.Orientation = Orientation.Vertical;
+            _panel.Background = Brushes.Azure;
+
+            _label = new TextBlock();
+            _label.Text = "Yash App:";
+            //_label.FontWeight. = new System.Windows.FontWeight() { };
+            _textBox = new TextBox();
+
+            _panel.Children.Add(_label);
+            _panel.Children.Add(_textBox);
+            _textBox.Text = "Hello from masterpiece!";
+
+            // Corrected Line:
+            base.Content = _panel; // Use base.Content to set the content of the UserControl.
+
+            
+        }
+        
+        
+
+
+    }
+}
