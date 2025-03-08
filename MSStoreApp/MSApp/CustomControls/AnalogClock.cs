4b5a4205a38f2f6d0eea86a878f436b2d6f201c5
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-07 17:03:17 -0800

    cc

---
+++ MSStoreApp/MSApp/CustomControls/AnalogClock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MSApp.CustomControls
+{
+    public class AnalogClock : Control
+    {
+        public AnalogClock()
+        {
+            TextBlock text = new TextBlock();
+            text.Text = "Hello, Welcome!";
+
+            Debug.WriteLine($"{this.Name}");
+
+            this.AddVisualChild(text);
+        }
+
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+
+        }
+    }
+
+}
