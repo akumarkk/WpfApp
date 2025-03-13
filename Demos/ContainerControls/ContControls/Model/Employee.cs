b9100a4bcdadb5935686d5825f887c780c000d91
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-12 19:23:03 -0700

    Employee model

---
+++ Demos/ContainerControls/ContControls/Model/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContControls.Model
+{
+    public class Employee
+    {
+        public string FirstName, LastName, Id;
+
+        public Employee()
+        {
+
+        }
+
+        public IEnumerable<Employee> GetInitialEmployees()
+        {
+            return new List<Employee>() { new Employee() { FirstName="Suraj", LastName="Dev", Id="10"},
+            new Employee() { FirstName="Riya", LastName="Dev", Id="101"},
+            new Employee() { FirstName="Vijay", LastName="Dev", Id="110"}
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} | {LastName} | {Id}";
+        }
+    }
+}
