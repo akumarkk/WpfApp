ca93c4456494e81749ad5972ea25d81fc578f2ce
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-13 16:20:37 -0700

    solid brush as resource

---
+++ Demos/Resources/ResRef/SolidBrushResourceWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResRef
+{
+    /// <summary>
+    /// Interaction logic for SolidBrushResourceWindow.xaml
+    /// </summary>
+    public partial class SolidBrushResourceWindow : Window
+    {
+        public SolidBrushResourceWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush br = new();
+            br.Color = Color.FromArgb(77, 77, 0, 0);
+            this.Resources["brush"] = br;
+        }
+
+        private void Rectangle_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush br = new();
+            br.Color = Color.FromArgb(77, 0, 77, 0);
+            this.Resources["brush"] = br;
+
+        }
+    }
+}
